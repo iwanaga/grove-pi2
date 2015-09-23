386ac7ba5e89879f17997717b955c8f021d1ea07
iwanaga <iwanaga@iij.ad.jp>
2015-09-23 17:03:16 +0900

    test scripts

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var GrovePi = require('node-grovepi').GrovePi;
+var Board = GrovePi.board;
+var AnalogSensor = GrovePi.sensors.Analog;
+
+var board = new Board({
+    debug: true,
+    onError: function(err) {
+        console.log(err)
+    },
+    onInit: function(res) {
+        if (res) {
+            console.log('GrovePi Version :: ' + board.version());
+
+            var humiditySensor = new AnalogSensor(0);
+            console.log('start reading');
+            humiditySensor.on('change', function(res) {
+                console.log(res);
+            });
+            humiditySensor.watch(1000);
+        }
+    }
+});
+
+board.init();
