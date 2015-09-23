258f653c7a4b5f9ae8b1a4b89fe2f8aac9e3e529
iwanaga <iwanaga@iij.ad.jp>
2015-09-23 17:32:26 +0900

    read temperature and humidity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var GrovePi = require('node-grovepi').GrovePi;
 var Board = GrovePi.board;
 var AnalogSensor = GrovePi.sensors.base.Analog;
+var TempHumidity = GrovePi.sensors.DHTDigital;
 
 var board = new Board({
     debug: true,
@@ -14,10 +15,14 @@ var board = new Board({
             console.log('connected to GrovePi');
 
             var humiditySensor = new AnalogSensor('A0');
+            var tempHumidity   = new TempHumidity('A1');
             console.log('start reading');
             setInterval(function(){
                 var value = humiditySensor.read();
-                console.log(value);
+                console.log('moisture', value);
+
+                var arr = tempHumidity.read();
+                console.log('temperature:', arr[0], 'humidity:', arr[1], 'heat index:', arr[2]);
             }, 1000);
         }
     }
