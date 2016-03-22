b0cc4a296301f5f9f09b05fe2ac4c3f13545d938
iwanaga <iwanaga@iij.ad.jp>
2016-03-22 22:47:31 +0900

    tmp

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
+var HI = require('heat-index');
+var post = require('./lib/http_post');
 var GrovePi = require('node-grovepi').GrovePi;
 var Board = GrovePi.board;
-var AnalogSensor = GrovePi.sensors.base.Analog;
 var DHTDigitalSensor = GrovePi.sensors.DHTDigital;
-var HI = require('heat-index');
 
 var board = new Board({
     debug: true,
@@ -13,18 +13,15 @@ var board = new Board({
     },
     onInit: function(res) {
         if (res) {
-            console.log('connected to GrovePi');
-
-            var humiditySensor = new AnalogSensor('A0');
-            var tempHumidity   = new DHTDigitalSensor(2, DHTDigitalSensor.VERSION.DHT22, DHTDigitalSensor.CELSIUS);
-            console.log('start reading');
+            var tempHumidity = new DHTDigitalSensor(2, DHTDigitalSensor.VERSION.DHT22, DHTDigitalSensor.CELSIUS);
             setInterval(function(){
-                var value = humiditySensor.read();
-                console.log('moisture', value);
-
                 var arr = tempHumidity.read();
-                console.log('temperature:', arr[0], 'humidity:', arr[1], 'heat index:', HI.heatIndex({temperature: arr[0], humidity: arr[1]}));
-            }, 1000);
+                post({
+                    temperature: arr[0],
+                    humidity: arr[1],
+                    heatIndex: HI.heatIndex({temperature: arr[0], humidity: arr[1]})
+                });
+            }, 60000);
         }
     }
 });
